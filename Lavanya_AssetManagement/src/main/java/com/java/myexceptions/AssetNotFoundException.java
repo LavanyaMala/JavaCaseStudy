7cc991a290e612fd0d18d08d1e43d980cdca057e
LavanyaMala <148610049+LavanyaMala@users.noreply.github.com>
2024-05-17 13:20:45 +0530

    Delete Lavanya_AssetManagement directory

--- Lavanya_AssetManagement/src/main/java/com/java/myexceptions/AssetNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.java.myexceptions;
-
-public class AssetNotFoundException extends Exception {
-    public AssetNotFoundException(String message) {
-        super(message);
-    }
-    
-}
--- Lavanya_AssetManagement/src/main/java/com/java/myexceptions/AssetNotMaintainedException.java
+++
@@ -1,7 +0,0 @@
-package com.java.myexceptions;
-
-public class AssetNotMaintainedException extends Exception {
-    public AssetNotMaintainedException(String message) {
-        super(message);
-    }
-}
